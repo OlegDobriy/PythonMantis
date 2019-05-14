ea82f0844fb4325a6aa6fd0c5510efdfe7cdd69b
OlegDobriy <46223141+OlegDobriy@users.noreply.github.com>
2019-05-14 23:52:07 +0300

    Тест добавление проекта

---
+++ test/test_add_project.py
@@ -0,0 +1,10 @@
+from model.project import Project
+
+
+def test_add_project(app):
+    old_list = app.project.get_projects_list()
+    project = Project(name='123', description='456')
+    app.project.create(project)
+    new_list = app.project.get_projects_list()
+    old_list.append(project)
+    pass
