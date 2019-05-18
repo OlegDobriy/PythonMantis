f406bce22fb1e02d823df64b1b1e96ad39daa241
OlegDobriy <46223141+OlegDobriy@users.noreply.github.com>
2019-05-18 16:44:48 +0300

    Тест создание нового аккаунта

---
+++ test/test_sign_up_new_account.py
@@ -0,0 +1,16 @@
+import string
+import random
+
+
+def random_username(prefix, maxlen):
+    symbols = string.ascii_letters
+    return prefix + ''.join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def test_sign_up_new_account(app):
+    username = random_username('user_', 10)
+    email = username + '@localhost'  # зависит от настроек почтового сервера, какие домены он выдает
+    password = 'test'
+    app.james.ensure_user_exists(username, password)
+    app.signup.new_user(username, email, password)
+    assert app.soap.can_login(username, password)
