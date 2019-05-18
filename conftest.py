7d848659aca296ee11e7b4afbc4709a94f4b0a73
OlegDobriy <46223141+OlegDobriy@users.noreply.github.com>
2019-05-18 16:43:59 +0300

    Изменение conftest

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ from fixture.application import Application
 import pytest
 import json
 import os.path
+import ftputil
 import importlib
 import jsonpickle
 
@@ -19,18 +20,32 @@ def load_config(file):
 
 
 @pytest.fixture
-def app(request):
+def app(request, config):
     global fixture
     browser = request.config.getoption('--browser')
     baseUrl = request.config.getoption('--baseUrl')
-    web_config = load_config(request.config.getoption('--target'))['web']
-    webadmin_config = load_config(request.config.getoption('--target'))['webadmin']# открыть конфиг из json, часть с web
+    web_config = config['web']
+    webadmin_config = config['webadmin']  # открыть конфиг из json, часть с web
     if baseUrl is None:  # если задали URL в параметре запуска, то брать его, иначе брать из target.json
         baseUrl = web_config['baseUrl']
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=baseUrl)
+        fixture = Application(browser=browser, config=config)
     fixture.session.check_login(username=webadmin_config['username'], password=webadmin_config['password'])
     return fixture
+
+"""
+@pytest.fixture(scope='session', autouse=True)
+def configure_server(request, config):
+    install_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+
+    def fin():
+        restore_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    request.addfinalizer(fin)
+"""
+
+@pytest.fixture(scope='session')
+def config(request):
+    return load_config(request.config.getoption('--target'))
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -43,6 +58,23 @@ def app_stop(request):
 
 
 def pytest_addoption(parser):
-    parser.addoption('--browser', action='store', default='firefox')
+    parser.addoption('--browser', action='store', default='chrome')
     parser.addoption('--baseUrl', action='store')
     parser.addoption('--target', action='store', default='target.json')
+
+
+def install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile('config_inc.php.bak'):
+            remote.remove('config_inc.php.bak')
+        if remote.path.isfile('config_inc.php'):
+            remote.rename('config_inc.php', 'config_inc.php.bak')
+        remote.upload(os.path.join(os.path.dirname(__file__), 'resources/config_inc.php'), 'config_inc.php')
+
+
+def restore_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile('config_inc.php.bak'):
+            if remote.path.isfile('config_inc.php'):
+                remote.remove('config_inc.php')
+                remote.rename('config_inc.php.bak', 'config_inc.php')
